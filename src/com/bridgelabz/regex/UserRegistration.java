8e68aa68768c4a6f479cf33836145084b1e8efa5
Pallavic5 <pchedge235@gmail.com>
2022-10-03 22:08:36 +0530

    Added: Check whether user enter a valid email or not

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -6,15 +6,32 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your First Name:");
+		String firstName = sc.next();
+		userFirstName(firstName);
 		System.out.println("Enter your Last Name:");
 		String lastName = sc.next();
 		userLastName(lastName);
+		System.out.println("Enter your email");
+		String email = sc.next();
+		userEmail(email);
 
 	}
 	
+	private static void userFirstName(String firstName) {
+		
+		UserInputValidation.isValidName(firstName);
+		
+	}
+
 	private static void userLastName(String lastName) {
 
 		UserInputValidation.isValidName(lastName);
+	}
+	
+	public static void userEmail(String email) {
+		
+		UserInputValidation.isValidEmail(email);
 	}
 
 }
