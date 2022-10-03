b9c02d97ce5febf1486275de8ed1713331dc6453
Pallavic5 <pchedge235@gmail.com>
2022-10-03 19:57:40 +0530

    Added: Enter a valid First Name

---
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.regex;
+
+import java.util.Scanner;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your First Name");
+		String firstName = sc.next();
+		userFirstName(firstName);
+
+	}
+
+	private static void userFirstName(String firstName) {
+		UserInputValidation.isValidFirstName(firstName);
+	}
+	
+	
+
+}
